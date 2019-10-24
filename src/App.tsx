f41ffb911e30302fb520cb1e56e699308797a1d5
Yuiki <10101661+Yuiki@users.noreply.github.com>
2019-10-24 17:42:23 +0900

    Persistent the courses state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Alert, Container, Navbar } from 'react-bootstrap';
 import './App.css';
 import RequirementSelector, { defaultRequirement } from './RequirementSelector';
-import RequirementsRootView, { emptyPlan } from './RequirementsRootView';
+import RequirementsRootView, { emptyPlan, Plan } from './RequirementsRootView';
+import RegistrationStatus from './RegistrationStatus';
+import Requirements, { RequirementWithChildren, RequirementWithCourses, SelectionRequirement } from './Requirements';
+
+const COURSES_STATE = "courses-state"
+
+type CoursesState = {[requirementTitle: string]: {code: string, status: RegistrationStatus}[]}
 
 const App = () => {
     const [requirement, setRequirement] = useState(defaultRequirement);
     const [plan, setPlan] = useState(emptyPlan);
+
+    if (plan === emptyPlan) {
+        loadStoredCoursesState(requirement, plan)
+    }
+
+    useEffect(() => {
+        if (plan === emptyPlan) {
+            return
+        }
+        storeCoursesState(plan)
+    }, [plan])
 
     return (
         <>
@@ -27,6 +44,61 @@ const App = () => {
             </Container>
         </>
     );
+}
+
+const storeCoursesState = (plan: Plan) => {
+    const coursesState = Array.from(plan.courseToRequirement.entries()).reduce((prev, [course, requirement]) => {
+        const status = plan.courseToStatus.get(course)
+        if (!status) {
+            return prev
+        }
+
+        const courseState = {
+            code: course.code,
+            status
+        }
+        const values = prev[requirement.name] || []
+        values.push(courseState)
+        return {...prev, [requirement.name]: values}
+    }, {} as CoursesState)
+
+    localStorage.setItem(COURSES_STATE, JSON.stringify(coursesState))
+}
+
+const loadStoredCoursesState = (rootRequirement: Requirements, plan: Plan) => {
+    const coursesStateJson = localStorage.getItem(COURSES_STATE)
+    if (!coursesStateJson || !(rootRequirement instanceof RequirementWithChildren)) {
+        return
+    }
+
+    const coursesStateGroupedByRequirementTitle = JSON.parse(coursesStateJson) as CoursesState
+
+    const flattenRequirements = rootRequirement.children.flatMap((child) => {
+        if (child instanceof RequirementWithCourses) {
+            return [child as RequirementWithCourses]
+        } else if (child instanceof SelectionRequirement) {
+            return child.options.map((option) => option.requirement)
+                     .filter((req) => req instanceof RequirementWithCourses) as RequirementWithCourses[]
+        }
+        return []
+    })
+
+    Object.entries(coursesStateGroupedByRequirementTitle).forEach(([requirementTitle, coursesState]) => {
+        console.log(requirementTitle)
+        const requirement = flattenRequirements.find((child) => child.name === requirementTitle)
+        if (!requirement) {
+            return
+        }
+
+        coursesState.forEach((courseState) => {
+            const course = requirement.courses.find((course) => course.code === courseState.code)
+             if (!course) {
+                 return
+             }
+            plan.courseToRequirement.set(course, requirement)
+            plan.courseToStatus.set(course, courseState.status)
+        })
+     })
 }
 
 export default App;
