0b93117802a5befccd403be31dba0dd684a22d31
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2019-11-15 13:57:29 +0900

    Updates

--- precache-manifest.d56ca0ff3632d14144feced8021830e4.js
+++ precache-manifest.d56ca0ff3632d14144feced8021830e4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd2df6173ff4896ed74a03be1b4312b7",
+    "revision": "8d048eb31ca7e1d352fa60afc265772b",
     "url": "./index.html"
   },
   {
-    "revision": "cc858cc1f59a3a298b2d",
+    "revision": "ecd4b1e69a272b7958ac",
     "url": "./static/css/2.619dee99.chunk.css"
   },
   {
-    "revision": "81dce792bb5db3c6f9f0",
+    "revision": "d0b5de15f818c3a29b9a",
     "url": "./static/css/main.14946916.chunk.css"
   },
   {
-    "revision": "cc858cc1f59a3a298b2d",
-    "url": "./static/js/2.d88060fb.chunk.js"
+    "revision": "ecd4b1e69a272b7958ac",
+    "url": "./static/js/2.5092e2b2.chunk.js"
   },
   {
-    "revision": "81dce792bb5db3c6f9f0",
-    "url": "./static/js/main.bae13b84.chunk.js"
+    "revision": "d0b5de15f818c3a29b9a",
+    "url": "./static/js/main.5633e357.chunk.js"
   },
   {
     "revision": "81c8f4a92d56f5926118",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ac943788bbe05a36c62e05f0b2026ef.js"
+  "./precache-manifest.d56ca0ff3632d14144feced8021830e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
