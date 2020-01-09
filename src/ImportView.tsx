6d08625077a35de5a550e96ce74861f94fe8c854
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2020-01-09 23:58:19 +0900

    [インポート] する前に確認ダイアログを表示する

--- src/ImportView.tsx
+++ src/ImportView.tsx
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
-import { Accordion, Button, Card, Form, useAccordionToggle } from "react-bootstrap";
+import { Accordion, Button, Card, Form, Modal, useAccordionToggle } from "react-bootstrap";
 import Course from "./Course";
+import getValueFromModal, { useModals } from './getValueFromModal';
 import Plan, { emptyPlan, fromJSON, toJSON } from "./Plan";
 import { RequirementWithCourses } from "./Requirements";
+
+const ImportConfirmationModal = ({ onReturn, onExited }: {
+    onReturn: (value: boolean) => void,
+    onExited: () => void
+}) => {
+    const [show, setShow] = useState(true);
+
+    return (
+        <Modal show={show} onHide={() => { setShow(false); onReturn(false); }} onExited={onExited}>
+            <Modal.Header closeButton>
+                <Modal.Title>インポート</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                インポートすると現在の設定状態は失われますが、よろしいですか？
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => { setShow(false); onReturn(false); }}>いいえ</Button>
+                <Button variant="danger" onClick={() => { setShow(false); onReturn(true); }}>はい</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
     eventKey: string,
@@ -12,6 +35,7 @@ const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
 }) => {
     const [jsonString, setJSONString] = useState("");
     const toggle = useAccordionToggle(eventKey, () => { });
+    const { modals, setModalsAndCount } = useModals();
 
     const nextPlan = (() => {
         try {
@@ -27,7 +51,10 @@ const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (nextPlan === undefined) {
+        if (
+            nextPlan === undefined ||
+            !await getValueFromModal(ImportConfirmationModal, {}, setModalsAndCount)
+        ) {
             return;
         }
         onSubmit(nextPlan);
@@ -36,6 +63,7 @@ const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
 
     return (
         <>
+            {modals}
             <Card>
                 <Card.Header>
                     <Accordion.Toggle eventKey={eventKey} variant="link" as={Button}>
@@ -58,7 +86,7 @@ const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
                                 />
                                 <Form.Control.Feedback type="invalid">JSONが不正です</Form.Control.Feedback>
                             </Form.Group>
-                            <Button variant="danger" type="submit" disabled={isInvalid}>インポート</Button>
+                            <Button type="submit" disabled={isInvalid}>インポート</Button>
                         </Form>
                     </Card.Body>
                 </Accordion.Collapse>
@@ -67,4 +95,4 @@ const ImportView = ({ eventKey, codeToCourse, nameToRequirement, onSubmit }: {
     );
 }
 
-export default ImportView
+export default ImportView;
