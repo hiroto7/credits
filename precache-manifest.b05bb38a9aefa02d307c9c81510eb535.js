b299aeb6e9a36db89d48a7b3b7f2633fa838c0c1
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2020-01-11 03:11:30 +0900

    Updates

---
+++ precache-manifest.b05bb38a9aefa02d307c9c81510eb535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6599416afe411f249c1e4ab731865306",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b9f42e2f7fec7dce2de",
+    "url": "./static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "6801a663508dbc408b09",
+    "url": "./static/css/main.66e4ee46.chunk.css"
+  },
+  {
+    "revision": "3b9f42e2f7fec7dce2de",
+    "url": "./static/js/2.a572d2a9.chunk.js"
+  },
+  {
+    "revision": "e5a0640eea52224ce77564421c11f724",
+    "url": "./static/js/2.a572d2a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6801a663508dbc408b09",
+    "url": "./static/js/main.0308a326.chunk.js"
+  },
+  {
+    "revision": "94734ac2268d3139fbe6",
+    "url": "./static/js/runtime-main.72a5ea55.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.677263cdf154cecd14c1c003fa3363eb.js"
+  "./precache-manifest.b05bb38a9aefa02d307c9c81510eb535.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
