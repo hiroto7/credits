2595d0159a7fa24dcd05c074c1ab1ebe88741b1b
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2020-02-29 00:51:12 +0900

    JSONファイルを直接エクスポート可能

--- src/ExportView.tsx
+++ src/ExportView.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Accordion, Button, Card, Form } from "react-bootstrap";
 import Plan, { toJSON } from "./Plan";
 
 const ExportView = ({ plan, eventKey }: {
     plan: Plan,
     eventKey: string,
-}) => (
+}) => {
+    const jsonText = JSON.stringify(toJSON(plan));
+    const [url, setURL] = useState<string | undefined>();
+    useEffect(() => {
+        const blob = new Blob([jsonText], { type: 'application/json' })
+        const url = URL.createObjectURL(blob);
+        setURL(url);
+        return () => URL.revokeObjectURL(url);
+    }, [jsonText]);
+
+    return (
         <Card>
             <Card.Header>
                 <Accordion.Toggle eventKey={eventKey} variant="link" as={Button}>
@@ -14,20 +24,21 @@ const ExportView = ({ plan, eventKey }: {
             </Card.Header>
             <Accordion.Collapse eventKey={eventKey}>
                 <Card.Body>
-                    <p>テキストボックスの内容をコピーして、テキストファイルなどに保存します。</p>
                     <Form>
-                        <Form.Group className="mb-0">
+                        <Form.Group>
                             <Form.Label>JSON</Form.Label>
                             <Form.Control
                                 readOnly className="input-monospace"
-                                value={JSON.stringify(toJSON(plan))}
+                                value={jsonText}
                                 onFocus={(e: React.FocusEvent<HTMLInputElement>) => e.target.select()}
                             />
                         </Form.Group>
+                        <Button as='a' href={url} download>保存</Button>
                     </Form>
                 </Card.Body>
             </Accordion.Collapse>
         </Card>
     );
+}
 
-export default ExportView
+export default ExportView;
