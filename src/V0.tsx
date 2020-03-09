de886c57dc4e786f5b35f144b22150d63abcec3a
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2020-03-10 07:58:07 +0900

    Plan の候補の選択方法を変更し、表示内容をリッチに
    再帰関数 f1 をジェネレーター関数に変更

--- src/V0.tsx
+++ src/V0.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button, ListGroup, Modal } from "react-bootstrap";
+import { Button, ListGroup, Modal, Badge } from "react-bootstrap";
 import Course from './Course';
-import Plan, { RegistrationStatus } from './Plan';
+import Plan, { RegistrationStatus, RegisteredCreditsCounts } from './Plan';
 import Requirements, { Range, RequirementWithCourses } from './Requirements';
 
 function* f0(
@@ -22,33 +22,22 @@ function* f0(
     } else {
         for (const [index, course] of unselectedCourses.entries()) {
             const slicedCourseList = unselectedCourses.slice(index + 1);
-            yield* f0(
+            const courseLists = f0(
                 requiredCreditsCount,
                 slicedCourseList,
                 [...selectedCourses, course],
                 selectedCreditsCountSum + course.creditsCount,
                 Math.min(selectedCreditsCountMin, course.creditsCount),
             );
+            yield* courseLists;
         }
     }
 }
 
-const f1 = (requirement: Requirements, requirements: readonly RequirementWithCourses[], plan: Plan): {
-    registered: { plan: Plan, creditsCount: number },
-    acquired: { plan: Plan, creditsCount: number },
-} | undefined => {
+function* f1(requirements: readonly RequirementWithCourses[], plan: Plan): Generator<Plan, void, undefined> {
     if (requirements.length === 0) {
-        const registeredCreditsCounts = requirement.getRegisteredCreditsCounts(plan, false);
-        return {
-            registered: {
-                plan,
-                creditsCount: registeredCreditsCounts.registered,
-            },
-            acquired: {
-                plan,
-                creditsCount: registeredCreditsCounts.acquired,
-            },
-        };
+        yield plan;
+        return;
     }
 
     const t0 = requirements.reduce<{
@@ -56,7 +45,7 @@ const f1 = (requirement: Requirements, requirements: readonly RequirementWithCou
         courseLists: readonly (readonly Course[])[],
     } | undefined>((previous, current) => {
         const courses0 = current.courses.filter(course => plan.courseToRequirement.get(course) === undefined && (plan.courseToStatus.get(course) ?? RegistrationStatus.Unregistered) !== RegistrationStatus.Unregistered);
-        let courseLists: (readonly Course[])[] = [];
+        const courseLists: (readonly Course[])[] = [];
         for (const courses of f0(current.creditsCount, courses0, [], 0, Infinity)) {
             if (previous !== undefined && courseLists.length + 1 >= previous.courseLists.length) {
                 return previous;
@@ -73,39 +62,67 @@ const f1 = (requirement: Requirements, requirements: readonly RequirementWithCou
         throw new Error();
     }
 
-    return t0.courseLists
-        .map(courses => {
-            const nextPlan: Plan = {
-                ...plan,
-                courseToRequirement: new Map([
-                    ...plan.courseToRequirement,
-                    ...courses.map(course => [course, t0.requirement] as const),
-                ])
-            };
-            return nextPlan;
-        })
-        .reduce<{
-            registered: { plan: Plan, creditsCount: number },
-            acquired: { plan: Plan, creditsCount: number },
-        } | undefined>((previous, current) => {
-            const plans = f1(requirement, requirements.filter(requirement => requirement !== t0.requirement), current);
-            if (plans === undefined) {
-                return previous;
-            } else {
-                if (previous === undefined) {
-                    return plans;
-                } else {
-                    return {
-                        registered: plans.registered.creditsCount > previous.registered.creditsCount ?
-                            plans.registered :
-                            previous.registered,
-                        acquired: plans.acquired.creditsCount > previous.acquired.creditsCount ?
-                            plans.acquired :
-                            previous.acquired,
-                    };
+    for (const courses of t0.courseLists) {
+        const plan0: Plan = {
+            ...plan,
+            courseToRequirement: new Map([
+                ...plan.courseToRequirement,
+                ...courses.map(course => [course, t0.requirement] as const),
+            ])
+        };
+        const plans = f1(requirements.filter(requirement => requirement !== t0.requirement), plan0)
+        yield* plans;
+    }
+}
+
+function* f2(requirement: Requirements, plan: Plan): Generator<readonly Plan[], void, undefined> {
+    const requirements = requirement.f(plan.selectionNameToOptionName);
+    const plan0 = { ...plan, courseToRequirement: new Map() };
+    let t0: readonly {
+        plan: Plan,
+        creditsCounts: RegisteredCreditsCounts,
+    }[] | undefined = undefined;
+    for (const plan1 of f1(requirements, plan0)) {
+        const creditsCounts = requirement.getRegisteredCreditsCounts(plan1, false);
+        if (t0 === undefined) {
+            t0 = [{
+                plan: plan1,
+                creditsCounts,
+            }];
+            yield [plan1];
+        } else {
+            let t1 = false;
+            const t2: {
+                plan: Plan,
+                creditsCounts: RegisteredCreditsCounts,
+            }[] = [];
+            for (const t3 of t0) {
+                if (!t1 && (
+                    creditsCounts.acquired > t3.creditsCounts.acquired ||
+                    creditsCounts.registered > t3.creditsCounts.registered
+                )) {
+                    t2.push({
+                        plan: plan1,
+                        creditsCounts,
+                    });
+                    t1 = true;
+                }
+                if ((
+                    t3.creditsCounts.acquired > creditsCounts.acquired ||
+                    t3.creditsCounts.registered > creditsCounts.registered
+                ) && (
+                        t3.creditsCounts.acquired === creditsCounts.acquired &&
+                        t3.creditsCounts.registered === creditsCounts.registered
+                    )) {
+                    t2.push(t3);
                 }
             }
-        }, undefined);
+            if (t1) {
+                t0 = t2;
+                yield t0.map(({ plan }) => plan);
+            }
+        }
+    }
 }
 
 const V0: React.FC<{
@@ -114,7 +131,7 @@ const V0: React.FC<{
     onSubmit: (plan: Plan) => void,
 }> = ({ requirement, plan, onSubmit }) => {
     const [show, setShow] = useState(false);
-    const [result, setResult] = useState<ReturnType<typeof f1>>(undefined);
+    const [plans, setPlans] = useState<readonly Plan[] | undefined>(undefined);
 
     return (
         <>
@@ -123,8 +140,9 @@ const V0: React.FC<{
                 onClick={
                     () => {
                         setShow(true);
-                        const requirements = requirement.f(plan.selectionNameToOptionName);
-                        setResult(f1(requirement, requirements, { ...plan, courseToRequirement: new Map() }));
+                        for (const p of f2(requirement, plan)) {
+                            setPlans(p);
+                        }
                     }
                 }
             >
@@ -136,18 +154,46 @@ const V0: React.FC<{
                 </Modal.Header>
                 <Modal.Body>
                     {
-                        result === undefined ? '要件を満たす割り当ては見つかりませんでした。' : (
-                            <ListGroup>
-                                <ListGroup.Item action onClick={() => { setShow(false); onSubmit(result.acquired.plan); }}>
-                                    修得 {result.acquired.creditsCount}
-                                </ListGroup.Item>
-                                <ListGroup.Item action onClick={() => { setShow(false); onSubmit(result.registered.plan); }}>
-                                    履修 {result.registered.creditsCount}
-                                </ListGroup.Item>
-                            </ListGroup>
+                        plans === undefined ? '要件を満たす割り当ては見つかりませんでした。' : (
+                            <>
+                                <p>要件を満たす割り当てが {plans.length} 個見つかりました。適用したいものを選択してください。</p>
+                                <ListGroup>
+                                    {
+                                        plans.map(plan1 => {
+                                            const status = requirement.getStatus(plan1);
+                                            const creditsCounts = requirement.getRegisteredCreditsCounts(plan1, false);
+                                            return (
+                                                <ListGroup.Item
+                                                    key={`${creditsCounts.acquired}-${creditsCounts.registered}`}
+                                                    action
+                                                    onClick={() => { setShow(false); onSubmit(plan1); }}
+                                                >
+                                                    <div className="d-flex justify-content-between align-items-center">
+                                                        <div>
+                                                            修得
+                                                            <> </>
+                                                            <strong className="text-success">{creditsCounts.acquired}</strong>
+                                                            <> / </>
+                                                            履修
+                                                            <> </>
+                                                            <strong className="text-primary">{creditsCounts.registered}</strong>
+                                                        </div>
+                                                        <Badge className="ml-2 flex-shrink-0" variant={status === RegistrationStatus.Acquired ? 'success' : status === RegistrationStatus.Registered ? 'primary' : 'secondary'}>
+                                                            {status === RegistrationStatus.Acquired ? '修得OK' : status === RegistrationStatus.Registered ? '履修OK' : '不足'}
+                                                        </Badge>
+                                                    </div>
+                                                </ListGroup.Item>
+                                            )
+                                        })
+                                    }
+                                </ListGroup>
+                            </>
                         )
                     }
                 </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShow(false)}>キャンセル</Button>
+                </Modal.Footer>
             </Modal>
         </>
     );
